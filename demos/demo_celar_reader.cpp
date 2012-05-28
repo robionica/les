4079bf64bc4ee1724f5d1a28d8c2ec5be8ac70db
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2012-05-28 08:59:05 +0300

    Improve demo for CELAR reader.

--- demos/demo_celar_reader.cpp
+++ demos/demo_celar_reader.cpp
@@ -6,48 +6,10 @@
 #include <boost/foreach.hpp>
 #include <boost/graph/graphviz.hpp>
 
-int
-main()
+void
+dump(const vector< set<int> >& U, const vector< set<int> >& S,
+     const vector< set<int> >& M)
 {
-  CELARReader reader = CELARReader();
-  reader.read("../problems/CELAR/scen01/VAR.TXT", /* var.txt */
-              "../problems/CELAR/scen01/DOM.TXT", /* dom.txt */
-              "../problems/CELAR/scen01/CTR.TXT", /* ctr.txt */
-              "../problems/CELAR/scen01/cst.txt"  /* cst.txt */
-              );
-  PackedMatrix cons_matrix = reader.get_cons_matrix();
-  //cons_matrix.dump();
-
-  MILPP problem;
-  cout << "Initialize new MILP problem with "
-       << cons_matrix.get_num_cols() << " cols and "
-       << cons_matrix.get_num_rows() << " constraints"
-       << endl;
-  problem.initialize(cons_matrix.get_num_cols(),
-                     cons_matrix.get_num_rows());
-  problem.set_cons_matrix(&cons_matrix);
-
-  /* Build interaction graph to check number of connected components. */
-  InteractionGraph g = InteractionGraph(&problem);
-  std::ofstream dotfile ("t.png");
-  write_graphviz (dotfile, g);
-
-  map< int, vector<int> > components = g.get_connected_components();
-
-  for (map< int, vector<int> >::iterator it = components.begin();
-       it != components.end(); it++)
-    {
-      cout << "Decompose connected component #"
-           << (*it).first
-           << endl;
-
-      /* Do finkelstein quasi-block decomposition and obtain
-         decomposition information */
-      vector< set<int> > U, S, M;
-      FinkelsteinQBDecomposition decomposer;
-      vector<int> initial_cols(1, (*it).second[0]);
-      decomposer.decompose(&problem, &initial_cols, &U, &S, &M, 10, true);
-
   for (size_t i = 0; i < U.size(); i++)
     {
       cout << " U"
@@ -87,9 +49,58 @@ main()
       std::cout << "}" 
                 << std::endl;
     }
+}
 
+int
+main(int argc, char* argv[])
+{
+  if (argc < 5)
+    {
+      cout << "USAGE: demo_celar_reader <VAR.TXT> <DOM.TXT> <CTR.TXT> <CST.TXT>"
+           << endl;
+      return 1;
     }
 
+  CELARReader reader = CELARReader();
+  reader.read(argv[1], /* var.txt */
+              argv[2], /* dom.txt */
+              argv[3], /* ctr.txt */
+              argv[4]  /* cst.txt */
+              );
+  PackedMatrix cons_matrix = reader.get_cons_matrix();
+  //cons_matrix.dump();
+
+  MILPP problem;
+  cout << "Initialize new MILP problem with "
+       << cons_matrix.get_num_cols() << " cols and "
+       << cons_matrix.get_num_rows() << " constraints"
+       << endl;
+  problem.initialize(cons_matrix.get_num_cols(),
+                     cons_matrix.get_num_rows());
+  problem.set_cons_matrix(&cons_matrix);
+
+  /* Build interaction graph to check number of connected components. */
+  InteractionGraph g = InteractionGraph(&problem);
+  //std::ofstream dotfile ("graph.png");
+  //write_graphviz(dotfile, g);
+
+  map< int, vector<int> > components = g.get_connected_components();
+
+  for (map< int, vector<int> >::iterator it = components.begin();
+       it != components.end(); it++)
+    {
+      cout << "Decompose connected component #"
+           << (*it).first
+           << endl;
+
+      /* Do finkelstein quasi-block decomposition and obtain
+         decomposition information */
+      vector< set<int> > U, S, M;
+      FinkelsteinQBDecomposition decomposer;
+      vector<int> initial_cols(1, (*it).second[0]);
+      decomposer.decompose(&problem, &initial_cols, &U, &S, &M, 10, true);
+      dump(U, S, M);
+    }
 
   return 0;
 }
