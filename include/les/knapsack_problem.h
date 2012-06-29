91910bb4c705a50229ce5057f28147f88790b5ac
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2012-06-28 18:11:17 -0700

    Use .hpp policy for C++ files.

--- include/les/knapsack_problem.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2012 Alexander Sviridenko
-
-#ifndef __LES_KNAPSACK_PROBLEM_H
-#define __LES_KNAPSACK_PROBLEM_H
-
-int fractional_knapsack(int* v, int* w, int max_w, unsigned n, int** result_x,
-                        int* result_w);
-
-#endif // __LES_KNAPSACK_PROBLEM_H
