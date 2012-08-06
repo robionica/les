254fd8a6cb64e6d8a6c91ec85cb1d2ca080f2344
d2rk <oleks.sviridenko@gmail.com>
2012-08-05 22:49:12 -0700

    Cleaup a code. Remove duplicates.

--- les/packed_matrix.cpp
+++ les/packed_matrix.cpp
@@ -1,8 +1,10 @@
 // Copyright (c) 2012 Alexander Sviridenko
+//
+// Implements packet_matrix.hpp interface.
 
 #include <les/packed_matrix.hpp>
 
-// Convert CoinPackedVector for coin-or library to PackedVector.
+// Converts CoinPackedVector for coin-or library to PackedVector.
 static void
 convert_CoinPackedVector_to_PackedVector(CoinShallowPackedVector* src,
                                          PackedVector* dst)
@@ -12,53 +14,43 @@ convert_CoinPackedVector_to_PackedVector(CoinShallowPackedVector* src,
   //  dst->insert(src->getIndices()[i], src->getElements()[i]);
 }
 
-/* FIXME: some sheet happens here. */
-PackedVector*
-PackedMatrix::get_vector(int i) const
+PackedVector* PackedMatrix::get_vector(int i) const
 {
-  if (i > get_num_rows())
-    {
-      return NULL;
-    }
+  if (i > get_num_rows()) {
+    return NULL;
+  }
   CoinShallowPackedVector src = slave_matrix_.getVector(i);
-  //if (!src)
-  //  return NULL;
   PackedVector* dst = new PackedVector();
   convert_CoinPackedVector_to_PackedVector(&src, dst);
   return dst;
 }
 
-vector<int>*
-PackedMatrix::get_nonzero_rows() const
+vector<int>* PackedMatrix::get_nonzero_rows() const
 {
   vector<int>* nonzero_rows = new vector<int>();
-  for (int i = 0; i < get_num_rows(); i++)
-    {
-      if (slave_matrix_.getVectorLengths()[i] > 0)
-        nonzero_rows->push_back(i);
+  for (int i = 0; i < get_num_rows(); i++) {
+    if (slave_matrix_.getVectorLengths()[i] > 0) {
+      nonzero_rows->push_back(i);
     }
+  }
   return nonzero_rows;
 }
 
-vector<int>*
-PackedMatrix::get_nonzero_cols() const
+vector<int>* PackedMatrix::get_nonzero_cols() const
 {
   map<int, bool> cols_map;
-  for (int i = 0; i < get_num_rows(); i++)
-    {
-      const int* indices = slave_matrix_.getIndices() +
-        slave_matrix_.getVectorStarts()[i];
-      for (int j = 0; j < slave_matrix_.getVectorLengths()[i]; j++)
-        {
-          cols_map[ indices[j] ] = true;
-        }
+  for (int i = 0; i < get_num_rows(); i++) {
+    const int* indices = slave_matrix_.getIndices() +
+      slave_matrix_.getVectorStarts()[i];
+    for (int j = 0; j < slave_matrix_.getVectorLengths()[i]; j++) {
+      cols_map[ indices[j] ] = true;
     }
-  /* Copy keys (cols) to special vector */
+  }
+  // Copy keys (cols) to special vector
   vector<int>* nonzero_cols = new vector<int>();
   for(map<int, bool>::iterator it = cols_map.begin();
-      it != cols_map.end(); ++it )
-    {
-      nonzero_cols->push_back( it->first );
-    }
+      it != cols_map.end(); ++it ) {
+    nonzero_cols->push_back( it->first );
+  }
   return nonzero_cols;
 }
