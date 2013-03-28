a37b4c1e57a653b8bddd705559ef57683d4bb2d9
Oleksandr Sviridenko <oleks.sviridenko@gmail.com>
2013-03-28 13:47:53 +0200

    Add fency demo.

---
+++ data/DEMO/demo1.mps
@@ -0,0 +1,24 @@
+NAME          DEMO1
+ROWS
+ N  OBJ
+ L  ROW1
+ L  ROW2
+ L  ROW3
+ L  ROW4
+COLUMNS
+    COL1      OBJ                  2   ROW1                 3
+    COL2      OBJ                  3   ROW1                 4
+    COL2      ROW2                 2   ROW3                 2
+    COL3      OBJ                  1   ROW1                 1
+    COL3      ROW2                 3   ROW4                 2
+    COL4      OBJ                  5   ROW2                 3
+    COL5      OBJ                  4   ROW3                 3
+    COL6      OBJ                  6   ROW4                 3
+    COL7      OBJ                  1   ROW4                 2
+RHS
+    RHS1      ROW1                 6   ROW2                 5
+    RHS1      ROW3                 4   ROW4                 5
+BOUNDS
+ UP BND1      ROW1                 1
+ LO BND1      ROW1                 0
+ENDATA
--- demos/draw_interaction_graph.py
+++ demos/draw_interaction_graph.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+#
+# USAGE: python draw_interaction_graph.py PROBLEM_FILENAME
 
 from __future__ import print_function
 
@@ -11,29 +13,49 @@ import pylab as plot
 from les.readers.mps_reader import MPSReader
 from les.interaction_graph import InteractionGraph
 
-def main():
-  if len(sys.argv) < 2:
-    print("Please provide input MPS problem", file=sys.stderr)
-    exit(0)
-  reader = None
-  filename = sys.argv[1]
+_EXT_READERS = {
+  ".mps": MPSReader
+}
+
+def read_problem(filename):
   if not os.path.exists(filename):
     raise IOError("File doesn't exist: %s" % filename)
+  root, ext = os.path.splitext(filename)
   if filename.endswith(".gz"):
-    stream = gzip.open(filename, 'rb')
-    reader = MPSReader()
-    reader.parse(stream)
-    stream.close()
+    stream = gzip.open(filename, "rb")
+    root, ext = os.path.splitext(root)
   else:
-    raise Exception()
-  p = reader.build_problem()
-  g = InteractionGraph(p)
+    stream = open(filename, "r")
+  reader_class = _EXT_READERS.get(ext)
+  if not reader_class:
+    raise Exception("Doesn't know how to read %s format. See available formats."
+                    % ext)
+  reader = reader_class()
+  reader.parse(stream)
+  stream.close()
+  return reader
+
+def draw_interaction_graph(g):
   pos = nx.spring_layout(g)
   nx.draw_networkx_nodes(g, pos, node_color="white")
   nx.draw_networkx_edges(g, pos, edge_color="black", arrows=True)
   nx.draw_networkx_labels(g, pos, font_family="sans-serif")
   plot.axis("off")
   plot.show() # or plot.savefig("decomposition_tree.png")
+
+def main():
+  if len(sys.argv) < 2:
+    print("USAGE: %s PROBLEM_FILENAME" % sys.argv[0], file=sys.stderr)
+    print()
+    print("Please provide input problem. "
+          "Supported problem formats are: %s" % _EXT_READERS.keys(),
+          file=sys.stderr)
+    exit(0)
+  try:
+    reader = read_problem(sys.argv[1])
+    draw_interaction_graph(InteractionGraph(reader.build_problem()))
+  except KeyboardInterrupt, e:
+    print("Interrupting...", file=sys.stderr)
 
 if __name__ == "__main__":
   main()
