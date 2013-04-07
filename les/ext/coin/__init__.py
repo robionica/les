2ebf747c73abd837c4fe7062a18c1e2655cd16db
Oleksandr Sviridenko <oleks.sviridenko@gmail.com>
2013-04-07 09:53:06 +0300

    Add OsiClpSolverInterfaceFactory.

--- les/ext/coin/__init__.py
+++ les/ext/coin/__init__.py
@@ -15,3 +15,4 @@
 from les.ext.coin.osi_sym_solver_interface import OsiSymSolverInterface
 from les.ext.coin.osi_sym_solver_interface_factory import OsiSymSolverInterfaceFactory
 from les.ext.coin.osi_clp_solver_interface import OsiClpSolverInterface
+from les.ext.coin.osi_clp_solver_interface_factory import OsiClpSolverInterfaceFactory
