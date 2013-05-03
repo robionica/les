2dd0ba744fbbd9db65ac078188732dc573ddb1f1
Oleksandr Sviridenko <oleks.sviridenko@gmail.com>
2013-05-03 13:19:33 +0300

    Add doc comments.

--- les/solvers/glp_solver.py
+++ les/solvers/glp_solver.py
@@ -16,12 +16,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""This module represents GLPK support. The GNU Linear Programming Kit (GLPK) is
+a software package intended for solving large-scale linear programming (LP),
+mixed integer linear programming (MILP), and other related problems. GLPK uses
+the revised simplex method and the primal-dual interior point method for
+non-integer problems and the branch-and-bound algorithm together with Gomory's
+mixed integer cuts for (mixed) integer problems.
+
+GLPK solver is represented by :class:`GLPSolver`.
+
+The follwoing code snippet shows how to use :class:`GLPSolver` to solve given
+`problem` (e.g. derived from :class:`~les.problems.bilp_problem.BILPProblem`)::
+
+  from les.solvers import glp_solver
+
+  # Turn off GLPK output
+  glp_solver.glpk.env.term_on = False
+  # Load and solve the problem
+  solver = glp_solver.GLPSolver()
+  solver.load_problem(problem)
+  solver.solve()
+"""
+
 import glpk
 import itertools
 
 from les.solvers.bilp_solver_base import BILPSolverBase
 
 class GLPSolver(glpk.LPX, BILPSolverBase):
+  """This class represents GLP solver based on :class:`glpk.LPX`."""
 
   def __init__(self, *args, **kwargs):
     glpk.LPX.__init__(self, *args, **kwargs)
@@ -29,6 +52,7 @@ class GLPSolver(glpk.LPX, BILPSolverBase):
     self._problem = None
 
   def load_problem(self, problem):
+    """Loads a given problem to the solver."""
     self.obj.maximize = problem.maximize
     self.rows.add(problem.get_num_rows())
     self.cols.add(problem.get_num_cols())
