0f35ce2be5abaf4184306fe85fed84c47861c6c5
Oleksandr Sviridenko <oleks.sviridenko@gmail.com>
2013-05-06 19:16:20 +0300

    Add name to constructor.

--- les/problems/problem_base.py
+++ les/problems/problem_base.py
@@ -14,6 +14,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""The :class:`ProblemBase` represents base model class that is used by all
+available models, such as :class:`~les.problems.bilp_problem.BILPProblem`,
+:class:`~les.problems.knapsack_problem.KnapsackProblem`, etc.
+"""
+
 import types
 
 class ProblemBase(object):
@@ -39,16 +44,22 @@ class ProblemBase(object):
     :raises: TypeError
     """
     if not type(name) is types.StringType:
-      raise TypeError()
+      raise TypeError("name must be a string: %s" % type(name))
     self._name = name
 
   def get_name(self):
-    """Returns the problem name. Return `DEFAULT_NAME` if name wasn't defined.
+    """Returns the problem name. Returns `DEFAULT_NAME` if name wasn't defined.
+
+    :returns: A string that represents problem's name.
     """
     return self._name or self.__class__.DEFAULT_NAME
 
   def build_subproblem(self, *args, **kwargs):
-    """Builds and returns subproblem for the current problem instance."""
+    """Builds and returns subproblem for the current problem instance. Pass all
+    the arguments to the subproblem's constructor.
+
+    :returns: A :class:`SubproblemBase` instance.
+    """
     raise NotImplementedError()
 
 class SubproblemBase(ProblemBase):
