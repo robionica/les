b3c87363a41711ec86c72de21a62bfd5a2337251
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2013-10-13 23:26:42 +0300

    Check or-tools pathes that will be included.

--- src/main/python/les/ext/google/operations_research/linear_solver/__init__.py
+++ src/main/python/les/ext/google/operations_research/linear_solver/__init__.py
@@ -1,5 +1,8 @@
 import os
 
-__path__.append(os.path.join(os.environ['OR_TOOLS_HOME_DIR'], 'src', 'gen',
-                             'linear_solver'))
-__path__.append(os.path.join(os.environ['OR_TOOLS_HOME_DIR'], 'lib'))
+for path in (os.path.join(os.environ['OR_TOOLS_HOME_DIR'], 'src', 'gen',
+                          'linear_solver'),
+             os.path.join(os.environ['OR_TOOLS_HOME_DIR'], 'lib')):
+  if not os.path.exists(path):
+    raise IOError("Path doesn't exist: %s" % path)
+  __path__.append(path)
